74fdf830706664db7b8f7a923550c822d821b106
Luís Fernando de Ávila <luisfernandoavila92@gmail.com>
2018-12-09 11:35:00 -0200

    Remover comentários

--- exe2.c
+++ exe2.c
@@ -51,36 +51,12 @@ int main(int argc, char *argv[]){
 	values_per_thread = num_values/num_threads;
 
 
-	/*
-	
-	for(t=0; t<NUM_THREADS; t++){
-		printf("Criando thread %d\n", t);
-
-		// int pthread_create(pthread_t * thread, pthread_attr_t * attr, void * (*start_routine)(void *), void * arg);
-		status = pthread_create(&threads[t], NULL, hello_w, (void *)t);
-		if (status) {
-			strerror_r(status,err_msg,LEN);
-			printf("Falha da criacao da thread %d (%d): %s\n",t,status,err_msg);
-			exit(EXIT_FAILURE);
-		} else {
-			printf("%lu criou thread %lu\n",pthread_self(),threads[t]);
-		}
-	}*/
-
-
 	int d, p = 0;
 	int v[values_per_thread];
 	for (int i = 0; i < num_threads; ++i)
 	{
-		/* code */
 		d = 0;
-		/*
-		for (int d; i < values_per_thread; ++d)
-		{
-			
-			v[d] = arg[p];
-			++p;
-		}*/
+		
 
 		while(d < values_per_thread){
 			v[d] = arg[p];
@@ -90,7 +66,6 @@ int main(int argc, char *argv[]){
 
 		printf("Criando thread %d\n", i);
 		int status = pthread_create(&threads[i], NULL, soma, (void *) &v[0]); 
-		//int status = pthread_create(&threads[i], NULL, soma, &arg[i]); //passo o vetor para soma na posiçõa em t
 		if(status){
 			strerror_r(status,err_msg,LEN);
 			printf("Falha da criacao da thread %d (%d): %s\n",i,status,err_msg);
@@ -98,7 +73,6 @@ int main(int argc, char *argv[]){
 		}else{
 			printf("Thread criada\n");
 			printf("%d\n", v[0]);
-			//p = p + values_per_thread;
 		}
 
 	}
